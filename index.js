bde7492b0a194187696f82f39907af8e2cd74054
Bryan Bosire <bryanbosire@gmail.com>
2022-06-14 10:52:28 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((callback) => {
+    const newCase = callback
+    .split(' ')
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+  return newCase
+  })
 }
